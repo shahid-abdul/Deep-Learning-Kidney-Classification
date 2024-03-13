e6f2ec1e6f4e0ef0228e60caa3d18bf7b161c56d
shahid-abdul <abdulshahidsyed7862@gmail.com>
2024-03-13 09:27:43 +0530

    modified commit

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -61,7 +61,7 @@ class ConfigurationManager:
         training = self.config.training
         prepare_base_model = self.config.prepare_base_model
         params = self.params
-        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney-CT-Scan-data")
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "CT-KIDNEY-DATASET-Normal-Cyst-Tumor-Stone")
         create_directories([
             Path(training.root_dir)
         ])
